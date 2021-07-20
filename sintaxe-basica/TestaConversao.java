2e5fe4a77dd1553175f29395c8e26337af7d269d
Lemuller-21 <86493061+Lemuller-21@users.noreply.github.com>
2021-07-20 19:43:18 -0300

    Add files via upload
    Adicionei os quinze arquivos que criei durante o curso

---
+++ sintaxe-basica/TestaConversao.java
@@ -0,0 +1,16 @@
+
+public class TestaConversao {
+
+	public static void main(String[] args) {
+		
+		//float pontoFlutuante = 3.14f;
+		
+		double salario = 1250.7;
+		int valor = (int) salario;
+		System.out.println(valor);
+		
+		//long numeroGrande = 32432423523L;
+		
+		System.out.println(0.2 + 0.1);
+	}
+}
